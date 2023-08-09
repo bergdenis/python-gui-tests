b60f998843e775205f342208635a3e120d0465b1
bergdenis <bergdenis@seznam.cz>
2023-08-09 21:03:25 +0200

    Реализовано генерация и чтение тестовых данных в формате Excel

--- generator/groups.py
+++ generator/groups.py
@@ -1,12 +1,38 @@
 from comtypes.client import CreateObject
+import getopt
+import string
+import random
+import sys
 import os
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 10
+f = "data/groups.xlsx"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
 
 project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 xl = CreateObject("Excel.Application")
 xl.Visible = 1
 wb = xl.Workbooks.Add()
-for i in range(10):
-    xl.Range["A%s" % (i + 1)].Value[()] = "group %s" % i
+for i in range(n):
+    group_name = random_string("group ", 10)
+    xl.Range["A%s" % (i + 1)].Value[()] = group_name
 wb.SaveAs(os.path.join(project_dir, "groups.xlsx"))
 xl.Quit()
